33cea46becfc1f510799264fb212843d5e08346c
Arthur <kyleg777@bu.edu>
2020-12-18 19:39:53 -0800

    Create Vigenere_CLI.c

---
+++ Vigenere_CLI.c
@@ -0,0 +1,19 @@
+/*
+ * Format: vignere 0-[encode, decode] 1-[message] 2-[key]
+ */
+
+#include <stdio.h>
+
+char* encode(char* message, char* key);
+char* decode(char* message, char* key);
+
+int main(int argc, char* argv[]){
+        if (argv[0] == "encode"){
+                
+        }
+        else if (argv[0] == "decode"){
+                
+        }
+
+        return 0;
+}
