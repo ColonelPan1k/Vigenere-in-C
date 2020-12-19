d00be17b627e5ac5e0bf8e7b2d1458f41cf12a44
Arthur <kyleg777@bu.edu>
2020-12-19 01:16:35 -0800

    Update Vigenere_CLI.c

--- Vigenere_CLI.c
+++ Vigenere_CLI.c
@@ -12,29 +12,66 @@
 //char* encode(char* message, char* key);
 //char* decode(char* message, char* key);
 
-
-
-static inline void
-caesarEncode(char* message, int shift)
+int
+wrapMod(int a, int b)
 {
-        int len = strlen(message);
-        
-        for (int i = 0; i < len; ++i){
-                message[i] = (((message[i] + shift - 97) % 26) + 97);                
+        if (a >= 0){
+                return (a % b);
+        } else {
+                return (a % b + b) % b;
+        }
+}
+
+static inline char
+caesarEncode(char message, int shift)
+{
+
+        return ((message + shift - 'a') % 26) + 'a';
+}
+
+static inline char
+caesarDecode(char message, int shift)
+{
+        char decode = wrapMod((message - shift - 97), 26) + 97;
+        return decode;
+}
+
+char*
+encode(char* message, char* key)
+{
+        int msgLen = strlen(message);
+        int keyLen = strlen(key);
+
+        for (int i = 0; i < msgLen; ++i){
+                message[i] = caesarEncode(message[i], key[i % keyLen] - 'a');
         }
 
-        printf("%s\n", message);
+        return message;
+}
+
+char*
+decode(char* message, char* key)
+{
+        int msgLen = strlen(message);
+        int keyLen = strlen(key);
+
+        for (int i = 0; i < msgLen; ++i){
+                message[i] = caesarDecode(message[i], (int)(key[i % keyLen]) - 97);
+        }
+        
+
+        return message;
 }
 
 int
 main(int argc, char** argv){
         
         if (strcmp("encode", argv[1]) == 0){
-                caesarEncode(argv[2], atoi(argv[3]));
+                printf("Encoded: %s\n", encode(argv[2], argv[3]));
 
         }
-        else if (argv[0] == "decode"){
-                //caesarDecode(argv[1], (int)argv[2]);
+        else if (strcmp("decode", argv[1]) == 0){
+                printf("Decoded: %s\n", decode(argv[2], argv[3]));
         }
 
         return 0;
